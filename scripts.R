1699dc09d79c07f068957bf55c52810c3fda065b
Mark Clements <scartomyzon@gmail.com>
2016-07-22 16:07:58 -0400

    digitized more specimens

--- scripts.R
+++ scripts.R
@@ -1,7 +1,5 @@
 library(StereoMorph)
 
-curves<-matrix(nrow = 3,ncol=3)
-curves[,1]<-c("body_ant","snout_tip","dorsal_ant")
 
 digitizeImage(image.file='Mox_images/left_side/',
               landmarks.ref='Mox_images/lmk_no_semi.txt',

abb9cc9a556cef2f05d5d499ebad5a2f6b6bbd1a
Mark Clements <scartomyzon@gmail.com>
2016-07-31 15:39:48 -0400

    more data

--- scripts.R
+++ scripts.R
@@ -5,3 +5,5 @@ digitizeImage(image.file='Mox_images/left_side/',
               landmarks.ref='Mox_images/lmk_no_semi.txt',
               shapes.file = 'Mox_images/shapes',
               curves.ref="Mox_images/outlines.txt")
+
+
